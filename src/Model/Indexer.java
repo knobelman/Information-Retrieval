9309444a7128d024ee69752ce07f61f8d8cfc265
MaorMa <maimonmaor@gmail.com>
2018-11-01 09:46:40 +0200

    Jsoup move to maven

--- src/Model/Indexer.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-/**
- * Created by Maor on 10/30/2018.
- */
-public class Indexer {
-}
--- src/Model/Tokenizer.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-/**
- * Created by Maor on 10/31/2018.
- */
-public class Tokenizer {
-    public Tokenizer() {
-    }
-}
